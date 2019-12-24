9af94a2f8b6a7e26fb7daa4381297cd58e2b1149
Colin Bates <ckabates@gmail.com>
2019-12-24 13:55:44 +1000

    Add project for challenge 21

---
+++ Day21/Models/IGiftRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace Day21.Models
+{
+    public interface IGiftRegistry
+    {
+        void Create();
+
+        void Add(string item);
+
+        void Close();
+    }
+}
---
+++ Day21/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Day21.Configurations;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Day21.Startup))]
+namespace Day21
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.Configure<GiftRegistryConfiguration>(config.GetSection("GiftRegistry"));
+        }
+    }
+}
